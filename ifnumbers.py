5019678a396862dd28552b6af72a11a3da7941e6
meetdonga2125 <mdcode2125@gmail.com>
2023-05-15 10:15:43 +0530

    Second Commit

---
+++ ifnumbers.py
@@ -0,0 +1,11 @@
+n = int(input("Enter a number: "))
+
+if(n>0):
+    print("It's a postitive number")
+elif(n<0):
+    print("It's a Negative number ")
+elif(n==0):
+    print("It's a zero")
+else:
+    print("I am not a number")    
+       
